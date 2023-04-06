c7b702aad64105b4fc2a4950a5f960c7285ff7b8
dangv31 <dangiraldova@unal.edu.co>
2023-04-06 12:57:57 -0500

    commit1

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,41 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma{
+    private String letras[];
+    private String interpretacion;
+
+    public Alfabeto(String origen, String[] letras,String interpretacion) {
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+    public int cantidadLetras(){
+        return letras.length;
+    }
+
+    @Override
+    String interpretacion() {
+        return interpretacion;
+    }
+
+    @Override
+    public String toString() {
+        return "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+}
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,42 @@
+package comunicacion;
+
+public class Fabula extends Escrito{
+    private String ensenanza;
+    private String interpretacion;
+
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza,String interpretacion){
+        super(origen, titulo, autor, paginas);
+        this.ensenanza=ensenanza;
+        this.interpretacion=interpretacion;
+    }
+
+    public int palabrasTotales(int num){
+        return num*getPaginas()*1;
+    }
+
+    @Override
+    String interpretacion() {
+        return interpretacion;
+    }
+
+    @Override
+    public String toString() {
+        return getOrigen()+"\n"+getTitulo()+"\n"+getAutor()+"\n"+getPaginas()+"\n"+ensenanza;
+    }
+
+    public String getEnsenanza() {
+        return ensenanza;
+    }
+
+    public void setEnsenanza(String ensenanza) {
+        this.ensenanza = ensenanza;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+}
