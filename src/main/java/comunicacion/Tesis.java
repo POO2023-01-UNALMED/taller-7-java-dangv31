fd029cf9d0c84282503ff19054180ab7dd617700
dangv31 <dangiraldova@unal.edu.co>
2023-04-06 13:05:45 -0500

    commit3

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -27,7 +27,7 @@ public class Tesis extends Escrito{
 
     @Override
     public String toString() {
-        return getOrigen()+"\n"+getTitulo()+"\n"+getAutor()+"\n"+getPaginas()+"\n"+idea+"\n"+argumentos+"\n"+conclusion+"\n"+referencias;
+        return getOrigen()+"\n"+getTitulo()+"\n"+getAutor()+"\n"+getPaginas()+"\n"+idea+"\n"+argumentos.length+"\n"+conclusion+"\n"+referencias;
     }
 
     public String getIdea() {
